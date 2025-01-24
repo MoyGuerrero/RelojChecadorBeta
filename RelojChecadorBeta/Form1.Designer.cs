6b73942752dd4a87a83aecb333ebc97faf9b57fe
MoyGuerrero <moisesguerrero907@gmail.com>
2025-01-23 21:46:57 -0600

    Agregar archivos de proyecto.

---
+++ RelojChecadorBeta/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace RelojChecadorBeta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblReloj = new System.Windows.Forms.Label();
+            this.imageEmploye = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageEmploye)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.BackColor = System.Drawing.SystemColors.Control;
+            resources.ApplyResources(this.lblReloj, "lblReloj");
+            this.lblReloj.Name = "lblReloj";
+            // 
+            // imageEmploye
+            // 
+            resources.ApplyResources(this.imageEmploye, "imageEmploye");
+            this.imageEmploye.Name = "imageEmploye";
+            this.imageEmploye.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.imageEmploye);
+            this.Controls.Add(this.lblReloj);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageEmploye)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblReloj;
+        private System.Windows.Forms.PictureBox imageEmploye;
+    }
+}
+
---
+++ RelojChecadorBeta/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelojChecadorBeta
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        public Form1()
+        {
+            timer = new Timer();
+            timer.Tick += new EventHandler(Form1_Load);
+            InitializeComponent();
+            timer.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximunForm();
+            Reloj();
+            isViewPicture();
+        }
+
+
+        public void MaximunForm()
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+        public void Reloj()
+        {
+            lblReloj.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        public void isViewPicture()
+        {
+            try
+            {
+                imageEmploye.ImageLocation = @"C:\Users\elzio\source\repos\RelojChecadorBeta\RelojChecadorBeta\Image\no-image.png";
+                imageEmploye.SizeMode = PictureBoxSizeMode.StretchImage;
+                // Crear un borde redondeado para el PictureBox
+                int borderRadius = 20; // Radio de los bordes redondeados
+                var graphicsPath = new GraphicsPath();
+                graphicsPath.AddArc(0, 0, borderRadius, borderRadius, 180, 90);
+                graphicsPath.AddArc(imageEmploye.Width - borderRadius, 0, borderRadius, borderRadius, 270, 90);
+                graphicsPath.AddArc(imageEmploye.Width - borderRadius, imageEmploye.Height - borderRadius, borderRadius, borderRadius, 0, 90);
+                graphicsPath.AddArc(0, imageEmploye.Height - borderRadius, borderRadius, borderRadius, 90, 90);
+                graphicsPath.CloseAllFigures();
+
+                // Asignar la región recortada al PictureBox
+                imageEmploye.Region = new Region(graphicsPath);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
